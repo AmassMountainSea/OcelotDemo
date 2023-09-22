db2cf06d7858dbbfa89ac5fac4f9ae8c8d6a6782
light714 <17615400466@163.com>
2023-09-22 10:36:54 +0800

    提交代码

---
+++ OcelotDemo.Auth/DataInit/ClientInitConfig.cs
@@ -0,0 +1,61 @@
+﻿
+using IdentityServer4;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace OcelotDemo.Auth.DataInit
+{
+    public class ClientInitConfig
+    {
+        /// <summary>
+        /// 定义ApiResource   
+        /// 这里的资源（Resources）指的就是管理的API
+        /// </summary>
+        /// <returns>多个ApiResource</returns>
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("Gateway")
+            };
+        }
+        /// <summary>
+        /// 定义ApiResource   
+        /// 这里的资源（Resources）指的就是管理的API
+        /// </summary>
+        /// <returns>多个ApiResource</returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("Gateway", "网关")
+                {
+                    Scopes = { "Gateway" }
+
+                }
+            };
+        }
+
+        /// <summary>
+        /// 定义验证条件的Client
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "TSP.client",//客户端惟一标识
+                    ClientSecrets = new [] { new IdentityServer4.Models.Secret("123456".Sha256()) },//客户端密码，进行了加密
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AccessTokenLifetime =  168 * 3600,  //过期时间
+                    //授权方式，客户端认证，只要ClientId+ClientSecrets 
+                    AllowedScopes = new [] { "Gateway",IdentityServerConstants.StandardScopes.OpenId, //必须要添加，否则报forbidden错误
+                    IdentityServerConstants.StandardScopes.Profile },//允许访问的资源
+              
+                }
+            };
+        }
+    }
+}
---
+++ OrderApi/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var project = _configuration.GetValue<string>("project");
+
+            var token = Convert.ToString(HttpContext?.Request.Headers["Authorization"]);
+            string info = "Order：" + project;
+
+            return info;
+
+
+        }
+    }
+}
